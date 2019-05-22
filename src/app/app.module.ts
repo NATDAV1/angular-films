54ef2c589f0d8d91a2cb735cc8fa8244874b9cc8
NATDAV1 <adn10siv@gmail.com>
2019-05-22 11:42:55 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesCategoryComponent } from './movies-category/movies-category.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ApiMoviesService } from './api-movies.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MoviesDisplayComponent } from './movies-display/movies-display.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesCategoryComponent,
+    MovieDetailComponent,
+    NavigationComponent,
+    MoviesDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiMoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
